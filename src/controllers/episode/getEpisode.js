0ccb23ae683a84a4a9413afb192446c241d82a25
AlmafuerteAriel <almafuerte.ariel.1978@gmail.com>
2023-09-28 15:57:01 -0300

    final push

--- src/controllers/episode/getEpisode.js
+++ src/controllers/episode/getEpisode.js
@@ -1,25 +1,22 @@
+const getActualUser = require("../../functions/getActualUser");
 const allEpisodes = require("../../utils/allEpisodes");
 
 const getEpisode = (req, res) => {
     const { email } = req.params;
     const { id } = req.query;
 
-    //* allEpisodes = [ {user1}, {user2}, {user3}, ... ]
-    //* actualUser = { email: user2, name, password, episodes: [ {1}, {2}, ...] }
-    //* filteredEpisode = [ { 1 } ]
+    const userEpisodes = getActualUser(req, res);
 
-    const actualUser = allEpisodes.find(user => user.email === email);
-    if(actualUser) {
-        if(id) {
-            const filteredEpisode = actualUser.episodes.filter(episode => episode.id === id);
-            if(filteredEpisode.length) {
-                return res.status(200).json(filteredEpisode);
-            }
-            return res.status(404).json({ message: "Episodio no encontrado"});
+    if (id) {
+        const filteredEpisode = userEpisodes.filter(
+            (episode) => episode.id === id
+        );
+        if (filteredEpisode.length) {
+            return res.status(200).json(filteredEpisode);
         }
-        return res.status(200).json(actualUser.episodes);
+        return res.status(404).json({ message: "Episodio no encontrado" });
     }
-    return res.status(404).json({ message: "Usuario no registrado"});
-}
+    return res.status(200).json(userEpisodes);
+};
 
 module.exports = getEpisode;
